183cd9d844815225fb039ea757c79b76173e6932
kamyllemaximo <kamylle.maximo@escola.pr.gov.br>
2026-06-25 08:00:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ document.querySelector("form").addEventListener("submit", function(event) {
 
     this.reset();
 });
+
+function mostrarMensagem() {
+    alert("A sustentabilidade no agronegócio é fundamental para garantir alimentos e preservar os recursos naturais.");
+}
+
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    alert("Mensagem enviada com sucesso!");
+
+    this.reset();
+});
+
+function mostrarMensagem() {
+    alert("A sustentabilidade no agronegócio é fundamental para garantir alimentos e preservar os recursos naturais.");
+}
+
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    alert("Mensagem enviada com sucesso!");
+
+    this.reset();
+});
